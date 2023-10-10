c702e3c2d9b04d8e7742283806dad6262d18f7a1
Yousuf <Yousuf@DESKTOP-H4U1I43>
2023-10-10 06:52:05 -0600

    SignalR_LiveChat

---
+++ SignalR_MidEvi/SignalR_MidEvi/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR_MidEvi
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.BroadcastMessage(name, message);
+        }
+
+        public void ShareFile(string name, string fileName, string fileData)
+        {
+            Clients.All.BroadcastFile(name, fileName, fileData);
+        }
+    }
+}
